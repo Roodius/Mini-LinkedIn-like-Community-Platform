0c13f998fe6ac421d99c005bc1536e8939f1e9dc
OsmanXwiinder <osmansaifi30@gmail.com>
2025-08-04 16:31:52 +0530

    new change

--- community-frontend/vite.config.ts
+++ community-frontend/vite.config.ts
@@ -4,11 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react(),
     tailwindcss()
   ],
    build: {
     outDir: 'dist'
-  }
+  },
+   base: './'  // <== THIS IS REQUIRED on Vercel
 })
